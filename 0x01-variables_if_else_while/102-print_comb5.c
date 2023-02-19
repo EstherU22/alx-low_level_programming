5facd4a8fc62b3435d36e2f766fff6af1bea67ed
Esther Onyeani <estherudensi77@gmail.com>
2023-02-19 15:30:16 -0800

    sft

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 	int o;
 
 	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
+{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
 		for (t = tens; t <= '9'; t++) /*print second of pair*/
@@ -29,8 +29,8 @@ int main(void)
 					if (!((tens == '9' && ones == '8') &&
 					      (t == '9' && o == '9')))
 					{
-						putchar(',');
-						putchar(' ');
+					putchar(',');
+					putchar(' ');
 					}
 				}
 				o = '0';

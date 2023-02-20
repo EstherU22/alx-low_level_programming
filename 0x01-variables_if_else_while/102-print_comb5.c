e4fde5bf9bc0d1612485335f6241539585769720
Esther Onyeani <estherudensi77@gmail.com>
2023-02-20 15:01:35 -0800

    soft ware is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
+ *main - prints all possible combinations of two two-digit numbers,
+ *ranging from 0-99, separated by a comma followed by a space.
+ *
+ *Return: Always 0.
  */
-
 int main(void)
 {
-	int i;
-	int j;
-
-	for (i = '0'; i < 99; i++) /*print first two digit combo*/
-{
-		for (j = 'i'; j <= 99 ; j++)
+	int num1, num2;
+	
+	for (num1 = 0; num1 <= 98; num1++) 
+	{	
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			if (i != 98 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-				}
-			}
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
+	}
+		
 	putchar('\n');
 
 	return (0);

985fe9842d881c06bb1771170fd8c139520e9522
Esther Onyeani <estherudensi77@gmail.com>
2023-02-20 17:31:39 -0800

    sof

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints all possible combinations of two two-digit numbers,
  * ranging from 0-99, separated by a comma followed by a space.
@@ -26,6 +25,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }

e8f14cb7f01776a3a2fbe673eeb6dba957055610
Esther Onyeani <estherudensi77@gmail.com>
2023-03-23 18:24:54 -0700

    variadic functions commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int sum = 0, i;
+
+	/*validate valist and initialise */
+	if (n == 0)
+		return (0);
+	va_start (valist, n);
+
+	/*iterate through list, update sum, free list */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+
+	return (sum);
+}

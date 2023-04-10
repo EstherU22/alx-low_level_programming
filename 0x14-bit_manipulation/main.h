7e65747f95a75bdc4272f3ee4279aa9581410f94
Esther Onyeani <estherudensi77@gmail.com>
2023-04-10 10:39:31 -0700

    updates

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,6 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h> /* NULL */
-
-/* helper function */
-int _putchar(char c);
 
 /* function prototypes for all files */
 unsigned int binary_to_uint(const char *b);
@@ -14,5 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _putchar(char c);
+int _atoi(const char *s);
 
 #endif

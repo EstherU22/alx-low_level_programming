84ab8ce50a52ecad000df508152d9df93be73907
Esther Onyeani <estherudensi77@gmail.com>
2023-04-11 10:37:44 -0700

    updates

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * flip_bits - determine how many bits to flip to get from one num to another
+ * flip_bits - determine how many bits to flip
+ * to get from one num to another
  * @n: number
- * @n: number2
+ * @m: number2
+ *
  * Return: how many bits differ
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int diff_bits = 0;
+	int i, diff_bits = 0;
 	unsigned long int difference;
+	unsigned long int exclusive = n ^ m;
+	
+	for (i = 63; i >= 0; i--)
+	{
+		differences = exclusive >> i;
+		if (differences & 1)
+			count++;
+	}
 
-	/* Xor both numbers to show bit 1 if different bits */
-	difference = n ^ m;
-
-	/* keep shifting difference to right and tallying the ones up */
-	do {
-		diff_bits += (differences & 1);
-		differences >>= 1;
-	} while
-		(difference > 0);
-
-	return (diff_bits);
+	return (count);
 }
